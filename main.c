5387f5dd6acb6055d5fb412e065729421f492fbd
hamzakhouzima <khouzimahamza20@gmail.com>
2022-09-07 11:56:50 +0100

    hhhhhh

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  int x;
+    printf("enter a number to check the sign :");
+  scanf("%d",&x);
+  if (x>0){
+printf("this is positive number ");
+    
+  }
+else if (x==0){
+
+   printf("null");
+}
+  else{
+printf("this is a negative number ");
+  }
+    return 0;
+}
+  
+  
